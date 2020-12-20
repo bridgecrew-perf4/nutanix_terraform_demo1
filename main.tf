59851cf045e85bd95f0351fe70db29eba77f66b8
Huimin Wang <433952+wanghm@users.noreply.github.com>
2020-12-20 11:22:15 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   /*
   backend "s3" {
     # Replace this with your bucket name!
-    bucket  = "devops-nutanix-terraform-state"
+    bucket  = "cluster1-nutanix-terraform-state"
     key     = "devops/s3/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
