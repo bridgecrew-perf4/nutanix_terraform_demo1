42fbc1396bff06b32d58bcd65c313fd83dc7ff2c
Huimin Wang <433952+wanghm@users.noreply.github.com>
2020-12-20 11:21:20 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,11 +7,11 @@ variable "vmname" {
 }
 
 variable "cluster_name" {
-  default = "DevOps-AOS5.17.1-auto_cluster_nested_5f50aeca82e14f9bf989ad2b"
+  default = "cluster1"
 }
 
 variable "subnet_name" {
-  default = "IPAM-10.129.44.0/24"
+  default = "subnet1"
 }
 
 variable "image_name" {
@@ -19,5 +19,5 @@ variable "image_name" {
 }
 
 variable "s3-backend-bucket" {
-  default = "devops-nutanix-terraform-state"
+  default = "cluster1-nutanix-terraform-state"
 }
